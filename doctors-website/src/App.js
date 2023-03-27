0c48bc4c07a460575b599367ea434303fd176b0f
Alexander Rusin <arusin@udel.edu>
2023-03-27 03:00:57 -0400

    grab data from doctors.js and display it in a table on the screen

--- doctors-website/src/App.js
+++ doctors-website/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import doctors from "./doctors";
 
 function App() {
+  const [selectedDoctor, setSelectedDoctor] = useState(null);
+
+  const handleClick = (index) => {
+    setSelectedDoctor(doctors[index]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List of Doctors</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Specialty</th>
+            <th>Location</th>
+            <th>Review Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {doctors.map((doctor, index) => (
+            <tr key={index} onClick={() => handleClick(index)}>
+              <td>{doctor.name}</td>
+              <td>{doctor.specialty}</td>
+              <td>{doctor.location}</td>
+              <td>{doctor.reviewScore}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
