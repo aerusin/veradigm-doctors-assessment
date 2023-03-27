cb4a929dbfd9d2159f8a67b2e818443599413b11
Alexander Rusin <arusin@udel.edu>
2023-03-27 02:46:58 -0400

    created a doctors json with all doctor data

---
+++ doctors-website/src/doctors.js
@@ -0,0 +1,50 @@
+const doctors = [
+  {
+    name: "Emily Lee",
+    specialty: "Dermatology",
+    location: "San Francisco",
+    reviewScore: 4.8,
+    details:
+      "Dr. Emily Lee is a dermatologist with expertise in cosmetic dermatology.",
+  },
+  {
+    name: "Michael Johnson",
+    specialty: "Oncology",
+    location: "Los Angeles",
+    reviewScore: 4.5,
+    details:
+      "Dr. Michael Johnson is an oncologist with over 15 years of experience in treating cancer patients.",
+  },
+  {
+    name: "Sarah Brown",
+    specialty: "Pediatrics",
+    location: "Chicago",
+    reviewScore: 4.7,
+    details:
+      "Dr. Sarah Brown is a pediatrician with expertise in child nutrition and development.",
+  },
+  {
+    name: "David Kim",
+    specialty: "Cardiology",
+    location: "New York",
+    reviewScore: 4.6,
+    details:
+      "Dr. David Kim is a cardiologist with a focus on heart disease prevention.",
+  },
+  {
+    name: "Rachel Chen",
+    specialty: "Neurology",
+    location: "San Francisco",
+    reviewScore: 4.9,
+    details:
+      "Dr. Rachel Chen is a neurologist with expertise in treating neurological disorders.",
+  },
+  {
+    name: "Jonathan Lee",
+    specialty: "Orthopedics",
+    location: "Los Angeles",
+    reviewScore: 4.4,
+    details:
+      "Dr. Jonathan Lee is an orthopedic surgeon with a focus on sports medicine.",
+  },
+];
